1d415fc71c8ebf8b869ccd42c4f0db2728319552
Alexander Dümont <alexander.duemont@sap.com>
2023-10-27 17:03:52 +0200

    Fix merge

--- srv/src/main/java/com/sap/cloud/sdk/demo/ad266/utility/Helper.java
+++ srv/src/main/java/com/sap/cloud/sdk/demo/ad266/utility/Helper.java
@@ -2,15 +2,13 @@ package com.sap.cloud.sdk.demo.ad266.utility;
 
 import cds.gen.goal.Goal101;
 import cds.gen.goalservice.Goal;
-import com.google.gson.JsonObject;
 import com.sap.cds.services.runtime.CdsRuntime;
 import com.sap.cloud.sdk.cloudplatform.connectivity.DestinationAccessor;
 import com.sap.cloud.sdk.cloudplatform.connectivity.DestinationProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.NoSuchElementException;
 
 @Component
 public class Helper
@@ -31,7 +29,8 @@ public class Helper
                 .getName();
 
         var email = DestinationAccessor.getDestination(destinationName)
-                .get(DestinationProperty.BASIC_AUTH_USERNAME).get();
+                .get(DestinationProperty.BASIC_AUTH_USERNAME)
+                .getOrElseThrow(() -> new NoSuchElementException("user"));
 
         return email.split("@")[0];
     }
