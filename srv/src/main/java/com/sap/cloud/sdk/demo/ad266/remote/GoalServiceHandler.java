d590822517f00b89cb16bd3fd7cf9d6e15176ffa
Johannes Schneider <johannes.schneider03@sap.com>
2023-10-30 15:57:19 +0100

    chore: revert changes to GoalServiceHandler

--- srv/src/main/java/com/sap/cloud/sdk/demo/ad266/remote/GoalServiceHandler.java
+++ srv/src/main/java/com/sap/cloud/sdk/demo/ad266/remote/GoalServiceHandler.java
@@ -1,13 +1,12 @@
 package com.sap.cloud.sdk.demo.ad266.remote;
 
-import cds.gen.goal.Goal_;
+/*import cds.gen.goal.Goal_;
 import cds.gen.goal.Goal101;
 import cds.gen.goal.GoalTask101;
 
 import static cds.gen.goal.Goal_.GOAL101;
 import static cds.gen.goal.Goal_.GOAL_TASK101;
-
-import cds.gen.goalservice.GoalService_;
+*/
 import com.sap.cds.ql.Insert;
 import com.sap.cds.ql.Select;
 import com.sap.cds.services.cds.CqnService;
@@ -38,35 +37,14 @@ import java.time.LocalDate;
 public class GoalServiceHandler implements EventHandler
 {
     @Autowired
-    @Qualifier(Goal_.CDS_NAME)
-    private CqnService goalService;
-
-    @Autowired
     private Helper helper;
 
-    public List<Goal101> getLearningGoals() {
+    /*
+    public List<Goal101> getLearningGoals()
+    {
         var user = helper.getUser();
 
-        var select = Select.from(GOAL101)
-                .columns(g -> g._all(),
-                        g -> g.tasks().expand(),
-                        g -> g.permissionNav().expand())
-                .where(g -> g.userId().eq(user)
-                        .and(g.category().eq("Learning and Growth"))
-                        .and(g.name().contains("Learn something at TechEd 2023"))
-                        .and(g.state().ne("Completed")));
-
-        var goals = goalService.run(select).listOf(Goal101.class);
-
-        var visibleGoals = goals
-                .stream()
-                .filter(g -> g.getPermissionNav().getView())
-                .toList();
-
-        log.info("Got the following goals from the server:");
-        visibleGoals.forEach(g -> log.info("ID: {}, Title: {}", g.getId(), g.getName()));
-
-        return visibleGoals;
+        return Collections.emptyList();
     }
 
     public Goal101 getLearningGoal() {
@@ -77,47 +55,23 @@ public class GoalServiceHandler implements EventHandler
         return createGoal(helper.getUser());
     }
 
-    private static Goal101 draftGoal(String user)
-    {
-        var goal = Goal101.create();
-
-        goal.setName(Helper.DEMO_ID + ": Learn something at TechEd 2023");
-        goal.setMetric("Attend sessions at TechEd 2023");
-        goal.setCategory("Learning and Growth");
-        goal.setType("user");
-        goal.setFlag(0);
-        goal.setWeight(0d);
-        goal.setUserId(user);
-        goal.setState("On Track");
-        goal.setStart(LocalDate.now());
-        goal.setDue(LocalDate.now().plusDays(14));
-        return goal;
-    }
-
     public Goal101 createGoal(String user)
     {
-        var draft = draftGoal(user);
-        var query = Insert.into(GOAL101).entry(draft);
 
-        var result = goalService.run(query).single(Goal101.class);
-
-        log.info("Created the following Goal in SFSF: {}", result);
-        return result;
     }
 
     public void createTask(Goal101 goal, String title )
     {
         var description = "Attend the session '" + title + "' and share what you learned!";
-        var task = GoalTask101.create();
-        task.setObjId(goal.getId());
-        task.setDescription(description);
-        task.setDone(10d);
 
-        var insert = Insert.into(GOAL_TASK101).entry(task);
-        goalService.run(insert);
     }
 
     public Result deleteGoal(CqnDelete delete){
         return goalService.run(delete);
     }
+
+    private static Goal101 draftGoal(String user)
+    {
+
+    }*/
 }
